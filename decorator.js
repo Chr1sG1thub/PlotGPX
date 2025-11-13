391e8fb9e042d86f4bbcf4dff5e91019de6adcbe
Chr1sG1thub <140241398+Chr1sG1thub@users.noreply.github.com>
2025-11-13 22:35:25 +0100

    Add files via upload

---
+++ decorator.js
@@ -0,0 +1,135 @@
+﻿/*! 
+ * Leaflet.PolylineDecorator - add symbols (arrows, patterns) on polylines or polygons
+ * https://github.com/bbecquet/Leaflet.PolylineDecorator
+ * (c) 2014 Benoit BECQUET
+ * License: MIT
+ */
+(function(factory, window) {
+  if (typeof define === "function" && define.amd) {
+    define(["leaflet"], factory);
+  } else if (typeof module === "object" && typeof module.exports === "object") {
+    module.exports = factory(require("leaflet"));
+  } else {
+    factory(window.L);
+  }
+})(function(L) {
+  "use strict";
+
+
+  L.Symbol = {};
+
+
+  L.Symbol.Marker = L.Class.extend({
+    options: {
+      rotate: true,
+      rotateOrigin: "center center",
+      markerOptions: {},
+      repeat: 0,
+      offset: 0
+    },
+    initialize: function(path, options) {
+      L.Util.setOptions(this, options);
+      this._path = path;
+    },
+    buildSymbol: function(map, latLng, angle) {
+      var marker = L.marker(latLng, this.options.markerOptions);
+      if (this.options.rotate) {
+        marker._icon.style.transformOrigin = this.options.rotateOrigin;
+        marker._icon.style[L.DomUtil.TRANSFORM + "Origin"] = this.options.rotateOrigin;
+        marker._icon.style[L.DomUtil.TRANSFORM] += " rotate(" + angle + "deg)";
+      }
+      return marker;
+    },
+    // Assume other necessary methods here...
+  });
+
+
+  L.Symbol.ArrowHead = L.Class.extend({
+    includes: L.Evented.prototype,
+    options: {
+      pixelSize: 10,
+      polygon: true,
+      pathOptions: {
+        stroke: true,
+        color: "black",
+        weight: 1,
+        opacity: 1,
+        fill: true,
+        fillOpacity: 1
+      }
+    },
+    initialize: function(options) { L.Util.setOptions(this, options); },
+    buildSymbol: function(map, latLng, angle) {
+      var pixelSize = this.options.pixelSize;
+      var pathOptions = this.options.pathOptions;
+
+
+      var arrowHead = L.polygon(this._buildArrowPoints(pixelSize), pathOptions)
+        .setRotationAngle(angle)
+        .setLatLng(latLng);
+
+
+      return arrowHead;
+    },
+    _buildArrowPoints: function(pixelSize) {
+      var r = pixelSize;
+      return [
+        L.point(0, 0),
+        L.point(-r, r / 2),
+        L.point(-r * 0.75, 0),
+        L.point(-r, -r / 2)
+      ];
+    }
+  });
+
+
+  L.polylineDecorator = function(paths, options) {
+    return new L.PolylineDecorator(paths, options);
+  };
+
+
+  L.PolylineDecorator = L.LayerGroup.extend({
+    initialize: function(paths, options) {
+      L.LayerGroup.prototype.initialize.call(this);
+      this._paths = paths;
+      L.Util.setOptions(this, options);
+      this._initPatterns();
+    },
+    onAdd: function(map) {
+      this._map = map;
+      this._initPatterns();
+      this._draw();
+      map.on("zoomend", this._draw, this);
+      map.on("moveend", this._draw, this);
+      this.addTo(map);
+    },
+    onRemove: function(map) {
+      this.clearLayers();
+      map.off("zoomend", this._draw, this);
+      map.off("moveend", this._draw, this);
+    },
+    _initPatterns: function() {
+      this._patterns = [];
+      var patternsOpts = this.options.patterns || [];
+      for (var i = 0; i < patternsOpts.length; i++) {
+        var pattern = patternsOpts[i];
+        var symbol = pattern.symbol;
+        this._patterns.push({
+          symbol: symbol,
+          offset: pattern.offset || 0,
+          repeat: pattern.repeat || 0
+        });
+      }
+    },
+    _draw: function() {
+      // Draw symbols along paths according to patterns
+      this.clearLayers();
+      this._patterns.forEach(function(pattern) {
+        // Assume implementation to draw symbols along the polyline
+      });
+    }
+  });
+
+
+  return L;
+});
---
+++ leaflet-gpx.js
@@ -0,0 +1,89 @@
+﻿/*!
+ * leaflet-gpx - A plugin to display GPX tracks on Leaflet maps
+ * Copyright (c) 2014, Mikael Petazzoni
+ * https://github.com/mpetazzoni/leaflet-gpx
+ */
+(function(factory) {
+  if (typeof define === "function" && define.amd) {
+    define(["leaflet"], factory);
+  } else if (typeof exports === "object") {
+    module.exports = factory(require("leaflet"));
+  } else {
+    factory(window.L);
+  }
+}(function(L) {
+  "use strict";
+  L.GPX = L.FeatureGroup.extend({
+    options: {
+      async: true,
+      marker_options: {
+        startIconUrl: "images/pin-icon-start.png",
+        endIconUrl: "images/pin-icon-end.png",
+        shadowUrl: "images/pin-shadow.png",
+        wptIconUrls: {
+          '': "images/pin-icon.png",
+          "0": "images/pin-icon.png",
+          "1": "images/pin-icon-1.png",
+          "2": "images/pin-icon-2.png",
+          "3": "images/pin-icon-3.png",
+          "4": "images/pin-icon-4.png",
+          "5": "images/pin-icon-5.png",
+          "6": "images/pin-icon-6.png",
+          "7": "images/pin-icon-7.png",
+          "8": "images/pin-icon-8.png",
+          "9": "images/pin-icon-9.png"
+        }
+      }
+    },
+    initialize: function(gpx, options) {
+      L.Util.setOptions(this, options);
+      this._gpx = gpx;
+      this._layers = {};
+      this._firefox = typeof InstallTrigger !== "undefined";
+      this._load();
+    },
+    load: function(url, options) {
+      options = options || {};
+      var xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = L.bind(function() {
+        if (xhr.readyState !== 4) {
+          return;
+        }
+        if (xhr.status === 200) {
+          this._parse(xhr.responseText);
+          this.fire("loaded");
+        } else if (xhr.status >= 400) {
+          this.fire("error", { status: xhr.status });
+        }
+      }, this);
+      xhr.open("GET", url, true);
+      xhr.send(null);
+    },
+    _load: function() {
+      if (typeof this._gpx === "string") {
+        this.load(this._gpx);
+      } else {
+        this._parse(this._gpx);
+        this.fire("loaded");
+      }
+    },
+    _parse: function(gpx) {
+      var xml = new DOMParser().parseFromString(gpx, "text/xml");
+      var trksegs = xml.getElementsByTagName("trkseg");
+      for (var i = 0; i < trksegs.length; i++) {
+        var trkpts = trksegs[i].getElementsByTagName("trkpt");
+        var latlngs = [];
+        for (var j = 0; j < trkpts.length; j++) {
+          var lat = parseFloat(trkpts[j].getAttribute("lat"));
+          var lon = parseFloat(trkpts[j].getAttribute("lon"));
+          latlngs.push(L.latLng(lat, lon));
+        }
+        var polyline = L.polyline(latlngs, { color: "blue", weight: 3, opacity: 0.75 });
+        this.addLayer(polyline);
+      }
+    }
+  });
+  L.gpx = function(gpx, options) {
+    return new L.GPX(gpx, options);
+  };
+}));
